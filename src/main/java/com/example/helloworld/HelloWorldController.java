fa82bb0ab97df27da6164db84bbae28c5b59c5b2
gotokaka <kakaporider510@gmail.com>
2023-05-12 13:22:50 +0900

    `@RequestParam`アノテーションを追加
    4行目-import文`RequestParam`
    10,11行目-`@RequestParam`アノテーション

--- src/main/java/com/example/helloworld/HelloWorldController.java
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -1,12 +1,13 @@
 package com.example.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
   @GetMapping("/hello")
-  public String hello() {
-    return "hello 後藤くん！！！！";
+  public String hello(@RequestParam(value = "name", defaultValue = "kakapo") String name) {
+    return String.format("<h1>Hello %s!</h1>", name);
   }
 }
