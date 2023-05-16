3c4f2a66731bd186e4e96b3a74798e4d998ed13e
gotokaka <kakaporider510@gmail.com>
2023-05-16 14:57:02 +0900

    現在時刻を表示する機能を追加
    `HelloWorldController`クラスの12-19行目に`nowTime`メソッドを追加。

--- src/main/java/com/example/helloworld/HelloWorldController.java
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -4,8 +4,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @RestController
 public class HelloWorldController {
+  @GetMapping("/nowTime")
+  public static String nowTime() {
+    LocalDateTime nowTime = LocalDateTime.now();
+    DateTimeFormatter formattingTime = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mmです");
+    String formattedTime = formattingTime.format(nowTime);
+    return "Hello World!! 現在日時は" + formattedTime;
+  }
+
   @GetMapping("/hello")
   public String hello(@RequestParam(value = "name", defaultValue = "kakapo") String name) {
     return String.format("<h1>Hello %s!</h1>", name);
